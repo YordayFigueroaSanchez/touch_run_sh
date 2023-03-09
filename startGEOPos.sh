bc184b6d08875ca78049325a2fcd661a90f665c9
yfsanchez <yfsanchez@gmail.com>
2023-03-09 13:19:12 -0300

    si contiene la cadena
    si contiene la cadena para solucionar el final de linea en caso de formato windows

--- startGEOPos.sh
+++ startGEOPos.sh
@@ -7,7 +7,7 @@ export GEOPOSTOUCH=/home/geocom/geopostouch/current
 #Se carga en la propiedad BUSINESS_NAME_WORK_TYPE el valor configurado desde Geoconfigurator con el nombre de como va a trabajar el equipo que utilizara la version.
 BUSINESS_NAME_WORK_TYPE=$(grep 'states.team.work.mode' $GEOPOS_HOME/configuration/pos/pos-production.properties | cut -d'=' -f2)
 
-if [[ $BUSINESS_NAME_WORK_TYPE = "CAJA" ]] ; then
+if [[ $BUSINESS_NAME_WORK_TYPE = *"CAJA"* ]] ; then
 	#LINEA CAJA TRADICIONAL.
 	cd $CONFIGURATOR
 	. ./run-configuratorClient.sh $GEOPOS_HOME/docs/VersionGEOPosCaja.xml >>$GEOPOS_HOME/logs/startGEOPosOut.log
@@ -17,7 +17,7 @@ if [[ $BUSINESS_NAME_WORK_TYPE = "CAJA" ]] ; then
 	cd $GEOPOS_HOME
 	. ./runPosUnify.sh>>$GEOPOS_HOME/logs/startGEOPosOut.log
 else
-	if [[ $BUSINESS_NAME_WORK_TYPE = "SCO" ]] ; then
+	if [[ $BUSINESS_NAME_WORK_TYPE = *"SCO"* ]] ; then
 		#LINEA SELFCHECKOUT (SCO).
 		cd $CONFIGURATOR
 		. ./run-configuratorClient.sh $GEOPOS_HOME/docs/VersionGEOPosCaja.xml >>$GEOPOS_HOME/logs/startGEOPosOut.log
@@ -37,7 +37,7 @@ else
 		cd $SELFCHECKOUT
 		. ./start.sh>>$SELFCHECKOUT/logs/start.log
 	else
-		if [[ $BUSINESS_NAME_WORK_TYPE = "TOUCH" ]] ; then
+		if [[ $BUSINESS_NAME_WORK_TYPE = *"TOUCH"* ]] ; then
 			#LINEA CAJA TOUCH.
 			cd $CONFIGURATOR                                                   
 			. ./run-configuratorClient.sh $GEOPOS_HOME/docs/VersionGEOPosCaja.xml >>$GEOPOS_HOME/logs/startGEOPosOut.log
